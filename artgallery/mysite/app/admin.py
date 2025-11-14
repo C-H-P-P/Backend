7813fb497924f70f578f0783fe9dd763eee3ef38
C_H_P <kachandmytro20@gmail.com>
2025-11-14 14:12:45 +0200

    Update admin.py

--- artgallery/mysite/app/admin.py
+++ artgallery/mysite/app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Gallery
 
-# Register your models here.
+@admin.register(Gallery)
+class GalleryAdmin(admin.ModelAdmin):
+    list_display = ('name_ua', 'name_en', 'created_at')
+    search_fields = ('name_ua', 'name_en')
+    readonly_fields = ('created_at', 'updated_at')
