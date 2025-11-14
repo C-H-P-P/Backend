9be257346074ae6454fb4cc0f660145966c468df
C_H_P <kachandmytro20@gmail.com>
2025-11-14 14:13:10 +0200

    Update models.py

--- artgallery/mysite/app/models.py
+++ artgallery/mysite/app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Gallery(models.Model):
+    name_ua = models.CharField(max_length=200, verbose_name="Назва (UA)")
+    name_en = models.CharField(max_length=200, verbose_name="Name (EN)")
+    description_ua = models.TextField(blank=True, verbose_name="Опис (UA)")
+    description_en = models.TextField(blank=True, verbose_name="Description (EN)")
+    image = models.ImageField(upload_to='gallery/', verbose_name="Зображення")
+    url = models.URLField(blank=True, null=True, verbose_name="Посилання")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name_ua or self.name_en
+
+    class Meta:
+        db_table = 'public_gallery'
+        verbose_name = "Gallery"
+        verbose_name_plural = "Galleries"
