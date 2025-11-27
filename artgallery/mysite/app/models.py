7915e97c01f6f07108198c8e2d4b9bf90a23cbb6
C_H_P <kachandmytro20@gmail.com>
2025-11-27 18:24:10 +0200

    Update models.py

--- artgallery/mysite/app/models.py
+++ artgallery/mysite/app/models.py
@@ -1,30 +1,21 @@
 from django.db import models
 
 
-class Artist(models.Model):
-    name = models.CharField("Ім'я художника", max_length=200)
-    bio = models.TextField("Біографія", blank=True)
-
-    def __str__(self):
-        return self.name
-    
-
-
+# *** ВИДАЛЕНО: Клас Artist видалено, оскільки дані керуються через Contentful ***
     
 class Gallery(models.Model):
+    # Залишаємо лише базові поля
     name_ua = models.CharField(max_length=200, verbose_name="Назва (UA)")
     name_en = models.CharField(max_length=200, verbose_name="Name (EN)")
 
-    city_ua = models.CharField(max_length=100, blank=True, verbose_name="Місто (UA)")
-    city_en = models.CharField(max_length=100, blank=True, verbose_name="City (EN)")
-
-    description_ua = models.TextField(blank=True, verbose_name="Опис (UA)")
-    description_en = models.TextField(blank=True, verbose_name="Description (EN)")
+    # ВИДАЛЕНО поля: city_ua, city_en, description_ua, description_en, url
+    
     image = models.ImageField(upload_to='gallery/', verbose_name="Зображення")
-    url = models.URLField(blank=True, null=True, verbose_name="Посилання")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    artists = models.ManyToManyField(Artist, blank=True, verbose_name="Художники")
+    
+    # ВИДАЛЕНО: зв'язок ManyToMany з Artist
+    
     def __str__(self):
         return self.name_ua or self.name_en
 
