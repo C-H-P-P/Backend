ae00101038109dddabeb06eb8193c7d8722e6458
C_H_P <kachandmytro20@gmail.com>
2025-11-27 18:23:32 +0200

    Update urls.py

--- artgallery/mysite/mysite/urls.py
+++ artgallery/mysite/mysite/urls.py
@@ -3,16 +3,18 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework.routers import DefaultRouter
-from app.views import GalleryViewSet
+
+# ПЕРЕВІРКА: Якщо GalleryViewSet знаходиться у mysite/app/views.py, імпорт правильний
+from app.views import GalleryViewSet 
 
 router = DefaultRouter()
-router.register(r'galleries', GalleryViewSet)
+router.register(r'galleries', GalleryViewSet) # Task 1.7, 1.8
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
+    path('api/', include(router.urls)), # /api/galleries/
     
-    
-    path('api/auth/', include('dj_rest_auth.urls')),# АУТЕНТИФІКАЦІЯ
+    # Task 3.2: АУТЕНТИФІКАЦІЯ та РЕЄСТРАЦІЯ
+    path('api/auth/', include('dj_rest_auth.urls')),
     path('api/auth/registration/', include('dj_rest_auth.registration.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
