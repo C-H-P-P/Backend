78f5ebefb6c39cb201cfa2b53b0ba60c67546eec
C_H_P <kachandmytro20@gmail.com>
2025-11-27 18:24:00 +0200

    Update serializers.py

--- artgallery/mysite/app/serializers.py
+++ artgallery/mysite/app/serializers.py
@@ -1,26 +1,21 @@
 from rest_framework import serializers
-from .models import Gallery, Artist   # ← ДОДАЙ Artist тут!
+from .models import Gallery
 
 
-# Серіалізатор для художників
-class ArtistSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Artist
-        fields = ['id', 'name', 'bio']
-
+# *** ВИДАЛЕНО: ArtistSerializer більше не потрібен ***
 
 # Серіалізатор для галерей
 class GallerySerializer(serializers.ModelSerializer):
-    image = serializers.ImageField(max_length=None, use_url=True)
-    artists = ArtistSerializer(many=True, read_only=True)  # ← вкладені художники
+    image = serializers.ImageField(max_length=None, use_url=True, required=False) 
+    # ВИДАЛЕНО: artists = ArtistSerializer(...)
 
     class Meta:
         model = Gallery
         fields = [
-            'id', 'name_ua', 'name_en',
-            'city_ua', 'city_en',
-            'description_ua', 'description_en',
-            'image', 'url',
-            'artists',                    # ← тепер є!
+            'id', 
+            'name_ua', 
+            'name_en',
+            # ВИДАЛЕНО: city_ua, city_en, description_ua, description_en, url, artists
+            'image',
             'created_at', 'updated_at'
         ]
